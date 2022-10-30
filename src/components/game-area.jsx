5fba9b59c714c2057361990f861c99eac6a28a12
simon.ashbury <simon.ashbury@hopin.to>
2022-10-30 16:06:47 +0000

    adds hand sprites for player and opponent

--- src/components/game-area.jsx
+++ src/components/game-area.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
+import playerHandRockImg from '../assets/hand-player-rock.png';
+import opponentHandRockImg from '../assets/hand-opponent-rock.png';
 
 const Canvas = styled.canvas`
-  height: 720px;
-  width: 1280px;
+  // height: 720px;
+  // width: 1280px;
 `;
 
 function GameArea() {
@@ -15,6 +17,18 @@ function GameArea() {
 
     context.fillStyle = '#000000';
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+    const playerHandImg = new Image();
+    playerHandImg.onload = () => {
+      context.drawImage(playerHandImg, 0, 22, 70, 128);
+    };
+    playerHandImg.src = playerHandRockImg;
+
+    const opponentHandImg = new Image();
+    opponentHandImg.onload = () => {
+      context.drawImage(opponentHandImg, 200, 0, 94, 70);
+    };
+    opponentHandImg.src = opponentHandRockImg;
   }, []);
   return <Canvas ref={canvasRef} />;
 }
