00373cf5448b7984a69636c2e05a0662ad6c1666
simon.ashbury <simon.ashbury@hopin.to>
2022-11-02 21:20:08 +0000

    adds more variation to the background

--- src/hooks/use-canvas.js
+++ src/hooks/use-canvas.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import shadeColor from '../util';
 
 export const canvasWidth = 1280;
 export const canvasHeight = 720;
@@ -14,6 +15,10 @@ export default function useCanvas() {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     context.fillStyle = canvasFillStyle;
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    context.beginPath();
+    context.fillStyle = shadeColor(canvasFillStyle, 20);
+    context.ellipse(720, 700, 1000, 300, 0, 3, 0);
+    context.fill();
 
     scene.forEach((gameObject) => {
       gameObject.draw(context);
